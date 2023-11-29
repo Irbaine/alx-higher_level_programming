a8a6a562230880b65cae3651418b07ebe8a222c2
Irbaine <irbaine@yahoo.com>
2023-11-28 16:56:32 -0800

    Done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    i = str(i).zfill(2)
-    if i == '99':
-        print("{}".format(i))
-    else:
-        print("{}, ".format(i), end="")
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+print("{:02d}".format(99))
